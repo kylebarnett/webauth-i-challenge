82698cecccb8be65ac8c65f22247b1a0ff513836
Kyle Barnett <kbarnett23@gmail.com>
2019-12-04 12:29:41 -0700

    everything finally set up

---
+++ api/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+const userRouter = require('./users/userRouter');
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+server.use(cors());
+
+server.use('/api/users', userRouter)
+
+module.exports = server;
