33a698cdbf884ec2e756558f304f6835043a1841
Kyle Barnett <kbarnett23@gmail.com>
2019-12-04 13:45:07 -0700

    MVP met

--- api/server.js
+++ api/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
 const userRouter = require('./users/userRouter');
+const authRouter = require('./auth/authRouter');
 
 const server = express();
 
@@ -10,5 +11,6 @@ server.use(express.json());
 server.use(cors());
 
 server.use('/api/users', userRouter)
+server.use('/api/auth', authRouter)
 
 module.exports = server;
--- api/users/userRouter.js
+++ api/users/userRouter.js
@@ -1,12 +1,34 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs');
 const Users = require('./userModel');
 
-router.get('/', (req, res) => {
+router.get('/', restricted, (req, res) => {
   Users.find()
     .then(users => {
       res.json(users);
     })
     .catch(err => res.send(err));
 });
+
+function restricted(req, res, next) {
+  let { username, password } = req.headers;
+  if (username && password) {
+    Users.findBy({ username })
+    .first()
+      .then(user => {
+        if (user && bcrypt.compareSync(password, user.password)) {
+          next()
+        } else {
+          res.status(401).json({ message: 'Invalid credentials.' })
+        }
+      })
+      .catch(err => {
+        console.log(err)
+        res.status(500).json({ message: "Unexpected error." })
+      })
+  } else {
+    res.status(400).json({ message: 'Please provide username and password.' })
+  }
+}
 
 module.exports = router;
